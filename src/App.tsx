ed5847ac3bc61295e25b24d3001ee39054a78b00
coolp <cool.pomolov@ya.ru>
2022-05-04 17:02:20 +0300

    Lesson_2 commit add DeleteFunc

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import './App.css';
 import {Todolist} from "./Todolist";
-import {task1, task2} from "./MassiveObj";
+
 
 function App() {
+    let task1 = [
+        {id: 1, title: "React", isDone: true},
+        {id: 2, title: "Html", isDone: true},
+        {id: 3, title: "JS", isDone: false},
+    ]
+
+
+    const DeleteFunction = (id: number) => {
+        task1 = task1.filter(el => el.id != id)
+
+    }
     return (
         <div className="App">
-        <Todolist title={"What to learn"} massive={task1}/>
-        <Todolist title={"What to sing"} massive={task2}/>
-
+            <Todolist title={"What to learn"} massive={task1} removeFunction={DeleteFunction}/>
         </div>
-)
+    )
 
 }
 
