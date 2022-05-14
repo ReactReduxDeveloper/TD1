91fddaf2ed2451f2150d1f50d8cf41e10a9dd4df
ReactReduxDeveloper <cool.pomolov@ya.ru>
2022-05-14 20:39:22 +0300

    commit added lesson_3 yarn add uuid

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,39 @@
 import React, {useState} from 'react';
 import './App.css';
-import {Todolist} from "./Todolist";
+import {MassiveType, Todolist} from "./Todolist";
 
 export type FilteredType = "all" | "completed" | "active"
+
 function App() {
-    let [task, Settask] = useState([
+    let [task, Settask] = useState<Array<MassiveType>>([
         {id: 1, title: "React", isDone: true},
         {id: 2, title: "Html", isDone: true},
         {id: 3, title: "JS", isDone: false},
         {id: 4, title: "GrapghQL", isDone: false},
         {id: 5, title: "Tesla", isDone: false},
     ])
-let [filtered, Setfiltered] = useState<"all" | "active" | "completed">("all")
+    let [filtered, Setfiltered] = useState<FilteredType>("all")
     let FilteredTasks = task
     if (filtered === "active") {
-        FilteredTasks = task.filter(el=>el.isDone === false)
+        FilteredTasks = task.filter(el => !el.isDone)
     }
-if (filtered === "completed") {
-    FilteredTasks = task.filter(el=>el.isDone === true)
-}
-    const FilteredFunction = (value:FilteredType)=> {
+    if (filtered === "completed") {
+        FilteredTasks = task.filter(el => el.isDone)
+    }
+    const FilteredFunction = (value: FilteredType) => {
         Setfiltered(value)
 
     }
     const DeleteFunction = (id: number) => {
-        let FilteredTasks = task.filter(el => el.id != id)
+        let FilteredTasks = task.filter(el => el.id !== id)
         Settask(FilteredTasks)
 
     }
     return (
         <div className="App">
-            <Todolist title={"What to learn"} massive={FilteredTasks} removeFunction={DeleteFunction}/>
+            <Todolist title={"What to learn"} massive={FilteredTasks} removeFunction={DeleteFunction}
+                      FilteredFunction={FilteredFunction}/>
+
         </div>
     )
 
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
+import {FilteredType} from "./App";
 
 type PropsType = {
     title: string
     massive: Array<MassiveType>
     removeFunction: (id: number) => void
-    FilteredFunction:(value:)
+    FilteredFunction: (value: FilteredType) => void
 }
-type MassiveType = {
+export type MassiveType = {
     id: number
     title: string
     isDone: boolean
 }
 export const Todolist = (props: PropsType) => {
+    const tasksJsXElements = props.massive.map(el =>
+        <li key={el.id}>
+            <input type="checkbox" checked={el.isDone}/>
+            <span>{el.title}</span>
+            <button onClick={() => {
+                props.removeFunction(el.id)
+            }}>X
+            </button>
 
+        </li>)
     return (
         <div>
             <div>
@@ -22,22 +32,12 @@ export const Todolist = (props: PropsType) => {
                     <button>+</button>
                 </div>
                 <ul>
-                    {
-                        props.massive.map(el =>
-                            <li key={el.id}>
-                                <input type="checkbox" checked={el.isDone}/>
-                                <span>{el.title}</span>
-                                <button onClick={() => {props.removeFunction(el.id)
-                                }}>X
-                                </button>
-
-                            </li>
-                        )}
+                    {tasksJsXElements}
                 </ul>
                 <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
+                    <button onClick={() => props.FilteredFunction("all")}>All</button>
+                    <button onClick={() => props.FilteredFunction("active")}>Active</button>
+                    <button onClick={() => props.FilteredFunction("completed")}>Completed</button>
                 </div>
             </div>
         </div>
