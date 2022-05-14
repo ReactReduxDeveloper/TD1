140d9ba59107fe082be68536205ab736fb74c401
coolp <cool.pomolov@ya.ru>
2022-05-14 14:11:49 +0300

    Lesson_2 commit add News

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from "./Todolist";
 
-
+export type FilteredType = "all" | "completed" | "active"
 function App() {
-    let task1 = [
+    let [task, Settask] = useState([
         {id: 1, title: "React", isDone: true},
         {id: 2, title: "Html", isDone: true},
         {id: 3, title: "JS", isDone: false},
-    ]
+        {id: 4, title: "GrapghQL", isDone: false},
+        {id: 5, title: "Tesla", isDone: false},
+    ])
+let [filtered, Setfiltered] = useState<"all" | "active" | "completed">("all")
+    let FilteredTasks = task
+    if (filtered === "active") {
+        FilteredTasks = task.filter(el=>el.isDone === false)
+    }
+if (filtered === "completed") {
+    FilteredTasks = task.filter(el=>el.isDone === true)
+}
+    const FilteredFunction = (value:FilteredType)=> {
+        Setfiltered(value)
 
-
+    }
     const DeleteFunction = (id: number) => {
-        task1 = task1.filter(el => el.id != id)
+        let FilteredTasks = task.filter(el => el.id != id)
+        Settask(FilteredTasks)
 
     }
     return (
         <div className="App">
-            <Todolist title={"What to learn"} massive={task1} removeFunction={DeleteFunction}/>
+            <Todolist title={"What to learn"} massive={FilteredTasks} removeFunction={DeleteFunction}/>
         </div>
     )
 
